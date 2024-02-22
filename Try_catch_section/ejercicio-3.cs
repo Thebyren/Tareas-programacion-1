9c4df0b0a241d8d40711a22ace7cf4c7c905ef09
Thebyren <byronrena1@gmail.com>
2024-02-22 14:40:49 -0600

    se sgrego el tercer ejercicio

---
+++ Try_catch_section/ejercicio-3.cs
@@ -0,0 +1,21 @@
+using Internal;
+using System;
+public class ejercicio_1
+{
+  public static void Main()
+  {
+    try
+    {
+      Console.WriteLine("ingrese el precio del producto:");
+      double precioProducto= double.Parse(Console.ReadLine());
+      if(precioProducto > 100){
+        precioProducto = precioProducto * 0.9;
+      }
+      Console.WriteLine("su compra final es de:"+ precioProducto);
+    }
+    catch (System.Exception ex)
+    {
+      Console.WriteLine("usted a ingresado algo diferente a un numero entero");
+    }
+  }
+}
