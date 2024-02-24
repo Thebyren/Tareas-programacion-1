1dc08eb2b159bb74bc160b8e6d3605a327a68ada
byron pastor <99785073+Thebyren@users.noreply.github.com>
2024-02-24 10:17:05 -0600

    ejercicio 2 agregado

---
+++ ejercicio-2.cs
@@ -0,0 +1,12 @@
+using System;
+
+class ejercicio{
+ public static void Main(){
+  int sum=0;
+for(int n = 1; n<=10; n++){
+    sum+=n;
+    Console.WriteLine(n);
+  }
+Console.WriteLine("resultado: "+sum);
+}
+}
