752c1bb3cfe3c5a569a7396c775c5666c7c291cd
Thebyren <byronrena1@gmail.com>
2024-02-25 15:19:00 -0600

    ahora si funciona el while.

--- ejercicio-4.cs
+++ ejercicio-4.cs
@@ -1,14 +1,16 @@
-
 using System;
 
-class ejercicio{
-public static void Main(){
-  int sum=0, n=1;
-do{
-Console.WriteLine(n);
-sum = n;
-n++;
-}while(n%11==0 && n%7==0);
-  Console.WriteLine("resultado es: "+sum);
- }
+class ejercicio
+{
+  public static void Main()
+  {
+    int sum = 0, n = 1;
+
+    do
+    {
+      n++;
+    } while (!(n % 11 == 0 && n % 7 == 0));
+    sum = n;
+    Console.WriteLine("El primer número divisible entre 7 y 11 es: " + sum);
+  }
 }
