dbbf746d6f0ce641dc262a4975ca6b74816e5b4c
Thebyren <byronrena1@gmail.com>
2024-02-26 15:42:10 -0600

    se modifico la estructura de checked en las revisiones

--- ejercicio-7.cs
+++ ejercicio-7.cs
@@ -4,15 +4,15 @@ using System;
 class ejercicio
 {
   static string Check(bool valor){
-    return valor ? "X":"*";
+    return valor ? "✓":"✗";
   }
   static void Main()
   {
    bool malComportado = false;
-   int[] notas= {70,70,70,70,70,60};
-   Console.WriteLine(Check(notas.All(num =>num>=70)));
-   Console.WriteLine(Check(notas.Average() >=70));
-   Console.WriteLine(Check(!malComportado));
+   int[] notas= {70,70,70,70,70,70};
+   Console.WriteLine("todos los cursos aprovados-["+Check(notas.All(num =>num>=70))+"]");
+   Console.WriteLine("promedio mayor a 70--------["+Check(notas.Average() >=70)+"]");
+   Console.WriteLine("buen comportamiento--------["+Check(!malComportado)+"]");
    if(!(notas.All(num =>num>=70) && (notas.Average() >=70) && !malComportado)){
       Console.WriteLine("el estudiante no puede graduarse");
    }else{
