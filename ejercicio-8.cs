7d9d1788035d382dd57aa7a607061e2313c865d0
Thebyren <byronrena1@gmail.com>
2024-02-26 18:37:30 -0600

    se agregaron colores a la terminal, ademas de la confirmacion para pasarle los objetos de libros

--- ejercicio-8.cs
+++ ejercicio-8.cs
@@ -27,13 +27,43 @@ class Libro{
 class ejercicio
 {
   public static void Main(){
-    Usuario persona = new Usuario();
+    Libro libro_1 = new Libro("harry potter y el principe mestizo", "fantasia","no referencial", true);
+    Libro libro_2 = new Libro("cronicas de narnia. el leon, la bruja y el ropero ", "ficcion","no referencial", false);
+    Libro libro_3 = new Libro("el hobbit", "fantasia","referencial", false);
+    Libro libro_4 = new Libro("48 leyes del poder", "historia","referencial", true);
     Console.WriteLine("ingrese su nombre");
-    persona.Nombre= Console.ReadLine();
-    Console.WriteLine(persona.Nombre);
+    List<Libro> listaLibros =new List<Libro>();
+    listaLibros.Add(libro_1);
+    listaLibros.Add(libro_2);
+    listaLibros.Add(libro_3);
+    listaLibros.Add(libro_4);
+    string nombre = Console.ReadLine();
+    Console.WriteLine("ingrese su edad");
+    int edad = int.Parse(Console.ReadLine());
+    Console.WriteLine("es un usuario premiun? y/n:");
+    bool premiun = Console.ReadLine().ToLower()=="y";
+    Usuario persona = new Usuario(nombre,edad, premiun );
+
+    Console.WriteLine("pidiendo prestados los libros...");
+    foreach (var libro in listaLibros)
+    {
+      Console.WriteLine($"pidiendo libro {libro.Titulo} para {persona.Nombre}");
+      if(TomarPrestado(persona, libro)){
+        persona.LibrosPrestados.Add(libro);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"se presto el libro {libro.Titulo}");
+        Console.ResetColor();
+        
+      }else{
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"no se pudo prestar el libro {libro.Titulo}");
+        Console.ResetColor();
+      }
+    }
+
   }
   static bool TomarPrestado(Usuario usuario, Libro libro){
-    if(libro.genero =="ficcion"){
+    if(libro.Genero =="ficcion"){
       return true;
     }else{
       return false;
