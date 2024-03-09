90df5f79f41d9a61b1db4f0457a05fd42fd90d4d
Thebyren <byronrena1@gmail.com>
2024-03-09 09:31:26 -0600

    se agrego el ejercicio 3

---
+++ ejercicio-3.cs
@@ -0,0 +1,9 @@
+using System;
+
+class ejercicio{
+  public static void Main(){
+  Console.WriteLine("ingrese su anio de nacimiento");
+  int year = int.Parse(Console.ReadLine());
+  Console.WriteLine($"su edad es: {year}");   
+  }
+}
