5577e004058c5b823325ebcbc3718681b7ece8fc
Thebyren <byronrena1@gmail.com>
2024-03-09 09:31:19 -0600

    se agrego el ejercicio 2

---
+++ ejercicio-2.cs
@@ -0,0 +1,9 @@
+using System;
+
+class ejercicio{
+  public static void Main(){
+  Console.WriteLine("cual es su nombre");
+  string nombre = Console.ReadLine(); 
+  Console.WriteLine($"mucho gusto, {nombre}");  
+  }
+}
