4a58419c46e1c2e33218dfb83551b7d67e5589fd
byron pastor <99785073+Thebyren@users.noreply.github.com>
2024-03-16 09:54:42 -0600

    add foreach in cwl

--- ejercicios.cs
+++ ejercicios.cs
@@ -4,8 +4,9 @@ class ejercicio{
 static void ejercicio_1(){
     int[] calificaciones;
     calificaciones = new int[] {85,92,77,68,94};
-    Console.WriteLine(calificaciones[2]);
-    
+    foreach(int index in calificaciones){
+        Console.WriteLine(index);
+    }
 }
 static void Main(){
     ejercicio_1();
