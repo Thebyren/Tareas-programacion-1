724585548fdf892cc89d6cfd2521cbabbb6a931a
byron pastor <99785073+Thebyren@users.noreply.github.com>
2024-03-16 09:45:45 -0600

    Create ejercicio-1.cs

---
+++ ejercicio-1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class ejercicio{
+static void ejercicio_1(){
+    int[] calificaciones;
+    calificaciones = new int[] {85,92,77,68,94};
+    Console.WriteLine(calificaciones[2]);
+    
+}
+static void Main(){
+    ejercicio_1();
+}
