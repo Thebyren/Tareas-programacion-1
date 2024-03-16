cf2b12bcdd575e9cfcff8fe88a2a36c3e2b4c40f
byron pastor <99785073+Thebyren@users.noreply.github.com>
2024-03-16 10:05:00 -0600

    calcula el promedio de las calificaciones

--- ejercicios.cs
+++ ejercicios.cs
@@ -4,9 +4,11 @@ class ejercicio{
 static void ejercicio_1(){
     int[] calificaciones;
     calificaciones = new int[] {85,92,77,68,94};
+    float promedio =0;
     foreach(int index in calificaciones){
         Console.WriteLine(index);
     }
+    Console.WriteLine($"El promedio es de: {promedio/calificaciones.Length}");
 }
 static void Main(){
     ejercicio_1();
