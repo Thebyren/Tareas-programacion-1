4aa4c158e0e395ead0d86f4bb27fa09279934a5c
byron pastor <99785073+Thebyren@users.noreply.github.com>
2024-03-16 10:53:13 -0600

    se agrego el ejercicio 4

--- ejercicios.cs
+++ ejercicios.cs
@@ -24,7 +24,44 @@ static void ejercicio_1(){
     Console.WriteLine($" el promedio es de: {promedio/calificaciones.Length}");
     
 }
+static void ejercicio_2(){
+    int[] elementos = new int[]{50,20,80,90,60};
+    Console.WriteLine($"original array");
+    infor(elementos);
+    Array.Sort(elementos);
+    Console.WriteLine($"resort array");
+   infor(elementos);
+   
+    Array.Reverse(elementos);
+    Console.WriteLine($"reverse array");
+   infor(elementos);
+}
+static void ejercicio_3(){
+    int[] edades ={25,30,18,45,30};
+    infor(edades);
+    Console.WriteLine("buscando la edad 20 en edades");
+    Console.WriteLine($"la edad 30 esta en la posicion {Array.IndexOf(edades,30)} del array de edades");
+}
+static void ejercicio_4(){
+    int[] calificaciones={85,92,77,68,94};
+    infor(calificaciones);
+    Console.WriteLine("verificando si alguien aprovo el curso");
+    Console.WriteLine($"{Array.Exists(calificaciones, calif=> calif>= 60)}");
+}
+static void infor(int[] array){
+ foreach(int num in array){
+    Console.WriteLine(num);
+    }   
+}
+
 static void Main(){
+    Console.WriteLine("\n\n\nejercicio 1\n\n");
     ejercicio_1();
+    Console.WriteLine("\n\n\nejercicio 2\n\n");
+    ejercicio_2();
+    Console.WriteLine("\n\n\nejercicio 3\n\n");
+    ejercicio_3();
+    Console.WriteLine("\n\n\nejercicio 4\n\n");
+    ejercicio_4();
 }
 }
